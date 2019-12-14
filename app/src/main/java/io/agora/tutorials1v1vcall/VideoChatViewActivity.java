972f4ab3587811d8a3909fff7011d6cc65054ba8
samyak-jain <samtan106@gmail.com>
2019-12-15 02:00:18 +0530

    Added new activity

--- app/src/main/java/io/agora/tutorials1v1vcall/VideoChatViewActivity.java
+++ app/src/main/java/io/agora/tutorials1v1vcall/VideoChatViewActivity.java
@@ -1,6 +1,6 @@
 package io.agora.tutorials1v1vcall;
 
-import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,20 +8,17 @@ public class VideoChatViewActivity extends AppCompatActivity {
     private static final String TAG = VideoChatViewActivity.class.getSimpleName();
 
     private static final int PERMISSION_REQ_ID = 22;
-
-    // Permission WRITE_EXTERNAL_STORAGE is not mandatory
-    // for Agora RTC SDK, just in case if you wanna save
-    // logs to external sdcard.
-    private static final String[] REQUESTED_PERMISSIONS = {
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+    private String APPID;
+    private String channelName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_chat_view);
+
+        Intent intent = getIntent();
+        APPID = intent.getStringExtra("APPID");
+        channelName = intent.getStringExtra("CHANNEL");
     }
 
     @Override
